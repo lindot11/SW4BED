508bf6d159e8bf85fd55607636637d24ce644e81
Linda Nguyen <lindaspostboks@gmail.com>
2023-03-07 15:16:11 +0100

    handin start

---
+++ HandIn2_ModelManagement/WebApplication1/Data/ModelManagementDb.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ModelManagement.Models;
+
+namespace ModelManagement.Data
+{
+    public class ModelManagementDb : DbContext
+    {
+        public ModelManagementDb(DbContextOptions<ModelManagementDb> options) : base(options) { }
+
+        public DbSet<Model> Models => Set<Model>();
+        public DbSet<Job> Jobs => Set<Job>();
+        public DbSet<Expense> Expenses => Set<Expense>();
+
+    }
+}
---
+++ HandIn2_ModelManagement/WebApplication1/Program.cs
@@ -0,0 +1,33 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+using ModelManagement.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<ModelManagementDb>(opt => opt.UseInMemoryDatabase("ModelList"));
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
