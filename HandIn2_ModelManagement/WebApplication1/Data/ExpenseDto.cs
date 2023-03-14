4948ae5549c6e094b00f2bfdd626d0ded9daee7e
Linda Nguyen <lindaspostboks@gmail.com>
2023-03-14 09:11:13 +0100

    dtos

---
+++ HandIn2_ModelManagement/WebApplication1/Data/ExpenseDto.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ModelManagement.Data
+{
+	public class ExpenseDto
+	{
+		public long ExpenseId { get; set; }
+
+		[Column(TypeName = "decimal(9,2)")]
+		public decimal amount { get; set; }
+	}
+}
---
+++ HandIn2_ModelManagement/WebApplication1/Data/JobDto.cs
@@ -0,0 +1,25 @@
+﻿using ModelManagement.Models;
+using System.ComponentModel.DataAnnotations;
+
+namespace ModelManagement.Data
+{
+	public class JobDto
+	{
+		public long JobId { get; set; }
+
+		[MaxLength(64)]
+		public string? Customer { get;}
+
+		public DateTime StartDate { get; set; }
+
+		public int Days { get; set; }
+
+		[MaxLength(128)]
+		public string? Location { get; set; }
+
+		[MaxLength(2000)]
+		public string? Comments { get; set; }
+
+		public List<Model>? Models { get; set; }
+	}
+}
