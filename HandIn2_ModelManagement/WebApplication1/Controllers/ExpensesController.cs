fcfbe8894bcfc19d303fd566bd521c101e169f74
lauestlauest <76699249+lauestlauest@users.noreply.github.com>
2023-03-14 10:33:06 +0100

    ExpencesControler

---
+++ HandIn2_ModelManagement/WebApplication1/Controllers/ExpensesController.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using ModelManagement.Data;
+using ModelManagement.Models;
+
+namespace ModelManagement.Controllers
+{
+    public class ExpensesController : Controller
+    {
+        private readonly ModelManagementDb _context;
+
+        public ExpensesController(ModelManagementDb context)
+        {
+            _context = context;
+        }
+
+        // GET: Expenses
+        public async Task<IActionResult> Index()
+        {
+              return View(await _context.Expenses.ToListAsync());
+        }
+
+        // GET: Expenses/Details/5
+        public async Task<IActionResult> Details(long? id)
+        {
+            if (id == null || _context.Expenses == null)
+            {
+                return NotFound();
+            }
+
+            var expense = await _context.Expenses
+                .FirstOrDefaultAsync(m => m.ExpenseId == id);
+            if (expense == null)
+            {
+                return NotFound();
+            }
+
+            return View(expense);
+        }
+
+        // GET: Expenses/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Expenses/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("ExpenseId,ModelId,JobId,Date,Text,amount")] Expense expense)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(expense);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(expense);
+        }
+
+        // GET: Expenses/Edit/5
+        public async Task<IActionResult> Edit(long? id)
+        {
+            if (id == null || _context.Expenses == null)
+            {
+                return NotFound();
+            }
+
+            var expense = await _context.Expenses.FindAsync(id);
+            if (expense == null)
+            {
+                return NotFound();
+            }
+            return View(expense);
+        }
+
+        // POST: Expenses/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(long id, [Bind("ExpenseId,ModelId,JobId,Date,Text,amount")] Expense expense)
+        {
+            if (id != expense.ExpenseId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(expense);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ExpenseExists(expense.ExpenseId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(expense);
+        }
+
+        // GET: Expenses/Delete/5
+        public async Task<IActionResult> Delete(long? id)
+        {
+            if (id == null || _context.Expenses == null)
+            {
+                return NotFound();
+            }
+
+            var expense = await _context.Expenses
+                .FirstOrDefaultAsync(m => m.ExpenseId == id);
+            if (expense == null)
+            {
+                return NotFound();
+            }
+
+            return View(expense);
+        }
+
+        // POST: Expenses/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(long id)
+        {
+            if (_context.Expenses == null)
+            {
+                return Problem("Entity set 'ModelManagementDb.Expenses'  is null.");
+            }
+            var expense = await _context.Expenses.FindAsync(id);
+            if (expense != null)
+            {
+                _context.Expenses.Remove(expense);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ExpenseExists(long id)
+        {
+          return _context.Expenses.Any(e => e.ExpenseId == id);
+        }
+    }
+}
