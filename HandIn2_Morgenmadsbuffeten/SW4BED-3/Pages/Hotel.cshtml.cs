63b69a0ffd63424b898d8dbe858eeca8218ba84b
Jonas Landsfeldt <jonaslandsfeldt@gmail.com>
2023-04-18 15:55:39 +0200

    First push

--- HandIn2_Morgenmadsbuffeten/SW4BED-3/Pages/Hotel.cshtml.cs
+++ HandIn2_Morgenmadsbuffeten/SW4BED-3/Pages/Hotel.cshtml.cs
@@ -2,6 +2,8 @@ using System.ComponentModel.DataAnnotations;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using SW4BED_3.Data;
+using SW4BED_3.Models;
 
 namespace SW4BED_3.Pages
 {
@@ -17,11 +19,15 @@ namespace SW4BED_3.Pages
         [BindProperty]
         [DataType(DataType.Date)]
 		public  DateTime  DateTime{ get; set; }
+        public Reservations Reservation { get; set; }
        
 		public void OnPost()
         {
-	        @ViewData["Date"] = $"{DateTime}";
+	        ViewData["Date"] = DateTime.ToShortDateString();
+	        ViewData["RoomNumber"] = Reservation.RoomNumber;
+	        ViewData["Adults"] = Reservation.AdultsReservations;
+	        ViewData["Children"] = Reservation.KidsReservations;
         }
 
-    }
+	}
 }
